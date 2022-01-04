bc4a7509ce10639a0a816284164ae72c670bc0c2
Anand1294 <95375740+Anand1294@users.noreply.github.com>
2022-01-04 14:47:08 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	public void doSomething() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
+	}
 
 }
